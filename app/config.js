f1b9ceef78640190865ca5cd11bb448401e143e7
Ichii <ethan.thierry06@gmail.com>
2020-08-29 18:54:11 +0200

    ⚙️ Added Comments in config

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PREFIX: "hs.",
-    HSE_MANAGER_GUILD: "330298629728239617",
-    HSE_ROLE: "749292733000187904"
+    HSE_MANAGER_GUILD: "330298629728239617", // Testing Facility Guild
+    HSE_ROLE: "749292733000187904" // Testing Facility Role
 }
