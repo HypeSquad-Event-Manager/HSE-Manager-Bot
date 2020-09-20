6d987bfa8f8313132d0b8950dbc4053e7b79caa8
ScarVite <admin@scarvite.de>
2020-09-20 11:03:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const package = require("./package.json");
 // Start with Stylised console output.
 console.log(`Starting "\x1b[33m${package.name}\x1b[0m" app.\n\x1b[32mVersion:\x1b[0m ${package.version}\n\x1b[32mAuthor:\x1b[0m ${package.author}`); 
 
-// Start bot instance.
+// Start bot instance
 require("./app/app");
