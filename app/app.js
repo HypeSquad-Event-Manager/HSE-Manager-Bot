78ae9d3ef93f83210d99768fe64ccdb8333afb53
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-06-25 19:00:04 +0200

    Fix CodeFactor check

--- app/app.js
+++ app/app.js
@@ -15,7 +15,7 @@ module.exports = { bot };
 require("./core/commands");
 require("./core/events");
 const { Twitter } = require("./core/twitter");
-if (TOKENS.TWITTER.ACCESS_TOKEN = "" || TOKENS.TWITTER.ACCESS_TOKEN_SECRET == "" || TOKENS.TWITTER.CONSUMER_KEY == "" || TOKENS.TWITTER.CONSUMER_SECRET == "") {
+if (TOKENS.TWITTER.ACCESS_TOKEN == "" || TOKENS.TWITTER.ACCESS_TOKEN_SECRET == "" || TOKENS.TWITTER.CONSUMER_KEY == "" || TOKENS.TWITTER.CONSUMER_SECRET == "") {
     console.log("\x1b[33mWARN:\x1b[0m It seems your Twitter credentials are unset, are you in dev mode ? Please setup your credentials if you are in production.");
 } else {
     Twitter();
